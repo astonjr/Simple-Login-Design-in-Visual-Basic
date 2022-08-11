724cb76bf7988efe52d137650d0500ccd2dc3187
Aston Junior <Aston Junior@AJ>
2022-08-11 22:26:03 +0300

    Add project files.

---
+++ Hintsoninput/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_MouseEnter(sender As Object, e As EventArgs) Handles TextBox1.MouseEnter
+        If TextBox1.Text = "Enter username" Then
+            TextBox1.Text = ""
+            TextBox1.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub TextBox1_MouseLeave(sender As Object, e As EventArgs) Handles TextBox1.MouseLeave
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "Enter username"
+            TextBox1.ForeColor = Color.Gray
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_MouseEnter(sender As Object, e As EventArgs) Handles TextBox2.MouseEnter
+        If TextBox2.Text = "Enter Password" Then
+            TextBox2.Text = ""
+            TextBox2.PasswordChar = "*"
+            TextBox2.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub TextBox2_MouseLeave(sender As Object, e As EventArgs) Handles TextBox2.MouseLeave
+        If TextBox2.Text = "" Then
+            TextBox2.Text = "Enter Password"
+            TextBox2.PasswordChar = ""
+            TextBox2.ForeColor = Color.Gray
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.CheckState = CheckState.Checked Then
+            TextBox2.PasswordChar = ""
+        Else
+            TextBox2.PasswordChar = "*"
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "aston" Or TextBox2.Text = "aston" Then
+            MsgBox("Please fill all entries", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "Error Message")
+        End If
+    End Sub
+
+    Private Sub Button1_MouseEnter(sender As Object, e As EventArgs) Handles Button1.MouseEnter
+        Button1.BackColor = Color.Black
+        Button1.ForeColor = Color.White
+    End Sub
+
+    Private Sub Button1_MouseLeave(sender As Object, e As EventArgs) Handles Button1.MouseLeave
+        Button1.BackColor = Color.White
+        Button1.ForeColor = Color.Black
+    End Sub
+End Class
